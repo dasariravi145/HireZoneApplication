c45a8c54315474755dd4d746f9c523f2ddfc08e9
unknown <dasariravid145@gmail.com>
2025-09-02 23:51:55 +0530

    #34521 added hirezone added queries in JPA repository

--- src/main/java/com/hirezone/repo/JobSeekerApplyRepo.java
+++ src/main/java/com/hirezone/repo/JobSeekerApplyRepo.java
@@ -1,19 +1,20 @@
 package com.hirezone.repo;
 
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.hirezone.entities.JobPostActivity;
+import com.hirezone.entities.JobSeekarProfile;
 import com.hirezone.entities.JobSeekerApply;
 
+public interface JobSeekerApplyRepo extends JpaRepository<JobSeekerApply, Integer> {
 
-public interface JobSeekerApplyRepo extends JpaRepository<JobSeekerApply,Integer> {
 	
-	
-	/*
-	 * List<JobSeekerApply> findByUserId(JobSeekarProfile userId);
-	 * 
-	 * 
-	 * List<JobSeekerApply> findByJob(JobPostActivity job);
-	 */
+	  List<JobSeekerApply> findByUserId(JobSeekarProfile userId);
+	  
+	  
+	  List<JobSeekerApply> findByJob(JobPostActivity job);
+	 
 
 }
